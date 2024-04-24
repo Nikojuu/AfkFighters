0a228f0ecb2132517779c012fb29ccafda7d6cc0
Nikojuu <come_on12345@hotmail.com>
2024-04-25 00:04:02 +0300

    test deploy

--- src/app/animal/[slug]/page.tsx
+++ src/app/animal/[slug]/page.tsx
@@ -1,19 +1,19 @@
 import { Fighter } from "@/components/fight-board";
 import { getAllFighters, getFighter } from "@/services/services";
 
-export async function generateStaticParams() {
-  const allFighters = await getAllFighters();
+// export async function generateStaticParams() {
+//   const allFighters = await getAllFighters();
 
-  return allFighters.map((animal: Fighter) => ({
-    slug: animal.slug,
-  }));
-}
+//   return allFighters.map((animal: Fighter) => ({
+//     slug: animal.slug,
+//   }));
+// }
 
 const SingleAnimalPage = async ({ params }: { params: { slug: string } }) => {
   // fetch database
-  const character = await getFighter(params.slug);
+  // const character = await getFighter(params.slug);
 
-  console.log(character);
+  // console.log(character);
   return <div></div>;
 };
 export default SingleAnimalPage;
