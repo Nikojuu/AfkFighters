590a1c554ddfad15017daca148aaa03590902d1d
Nikojuu <come_on12345@hotmail.com>
2024-04-30 13:38:52 +0300

    cache update9

--- src/app/api/fighters/route.ts
+++ src/app/api/fighters/route.ts
@@ -1,6 +1,6 @@
+"use server";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   console.log("searchParams", searchParams);
