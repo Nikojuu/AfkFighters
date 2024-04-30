f765b9e402ddd6122e2fa1a8da3a623c8ddd6f17
Nikojuu <come_on12345@hotmail.com>
2024-04-30 12:53:30 +0300

    cache update

--- src/app/api/fighters/route.ts
+++ src/app/api/fighters/route.ts
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET(request: Request) {
   try {
     // Execute SQL query to select two random fighters from the database
