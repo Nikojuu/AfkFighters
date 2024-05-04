f442f2c2878e8340cc25dfca720b2ee9bd4c14eb
Nikojuu <come_on12345@hotmail.com>
2024-05-04 13:29:00 +0300

    bug fix

--- src/app/api/fight-logic/route.ts
+++ src/app/api/fight-logic/route.ts
@@ -81,7 +81,10 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
 
     return NextResponse.json(result);
   } catch (error) {
-    console.log(error);
+    return NextResponse.json(
+      { error: "An error occurred while processing the request." },
+      { status: 500 }
+    );
   }
 };
 
