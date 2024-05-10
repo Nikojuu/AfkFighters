d5e0d6a1aa5c95e44490130c986e95194083ffee
Nikojuu <come_on12345@hotmail.com>
2024-05-10 11:32:49 +0300

    more cache problems..

--- src/app/fighters/page.tsx
+++ src/app/fighters/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export interface FightersArray {
   fighters: Fighter[];
 }
-export const revalidate = 3600;
+export const revalidate = 0;
 const FighersPage = async () => {
   const allFighters = await getAllFighters();
 
