168cb0a49bdf0c1a3bde0076e0943693c0b4a008
Nikojuu <come_on12345@hotmail.com>
2024-05-10 11:36:42 +0300

    more cache problems..

--- src/app/api/all-fighters/route.ts
+++ src/app/api/all-fighters/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
+  console.log(searchParams);
   try {
     const query = sql`
     SELECT * FROM fighters
--- src/app/fighters/page.tsx
+++ src/app/fighters/page.tsx
@@ -7,7 +7,8 @@ import Link from "next/link";
 export interface FightersArray {
   fighters: Fighter[];
 }
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 const FighersPage = async () => {
   const allFighters = await getAllFighters();
 
